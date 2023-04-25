ffbb9bd0a531c82868d1ba5619e46cb5b446a344
TALEM Idir <idir.talem@yassir.com>
2023-04-25 02:17:38 +0100

    Commit

---
+++ src/main/java/com/yassir/logger/CustomEnhancer.java
@@ -0,0 +1,43 @@
+package com.yassir.logger;
+
+import ch.qos.logback.classic.spi.ILoggingEvent;
+
+import com.google.cloud.logging.LogEntry.Builder;
+import com.google.cloud.logging.logback.LoggingEventEnhancer;
+import com.google.cloud.logging.Payload;
+import com.google.cloud.logging.Severity;
+
+import java.util.HashMap;
+
+public class CustomEnhancer implements LoggingEventEnhancer {
+	
+	@Override
+    public void enhanceLogEntry(Builder builder, ILoggingEvent e) {
+        
+        builder.clearLabels();
+        builder.addLabel("domain", "value");
+        builder.addLabel("squad", "value");
+        builder.addLabel("environment", "value");
+
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("message", e.getMessage());
+        if(builder.build().getSeverity() == Severity.ERROR){
+
+            HashMap<String, Object> source = new HashMap<>();
+            source.put("lineNumber", e.getCallerData()[0].getLineNumber());
+            source.put("methodName", e.getCallerData()[0].getMethodName());
+            source.put("className", e.getCallerData()[0].getClassName());
+            source.put("fileName", e.getCallerData()[0].getFileName());
+
+            map.put("stack_trace", builder.build().getPayload().getData());
+            map.put("source", source);
+        }
+        
+        
+        builder.setPayload(
+            Payload.JsonPayload.of(map)
+        );
+        
+    }
+
+}
--- src/main/java/com/yassir/logger/Idir.java
+++
@@ -1,9 +0,0 @@
-package com.yassir.logger;
-
-public class Idir {
-	
-	public static void display() {
-		System.out.println("Hey it's me");
-	}
-
-}
